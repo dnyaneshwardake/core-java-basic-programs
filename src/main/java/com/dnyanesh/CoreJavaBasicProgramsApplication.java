2183d5972ff6ea2619122f26cb8a1d9f910e2295
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2021-02-11 23:04:22 -0500

    updated to maven

---
+++ src/main/java/com/dnyanesh/CoreJavaBasicProgramsApplication.java
@@ -0,0 +1,13 @@
+package com.dnyanesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJavaBasicProgramsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJavaBasicProgramsApplication.class, args);
+	}
+
+}
